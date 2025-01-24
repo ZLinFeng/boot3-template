e6b2c966ab9376be5244c036ce586908fd4fab6f
Zhu <1@qq.com>
2025-01-24 14:59:22 +0800

    first commit

---
+++ src/main/java/com/berry/server/App.java
@@ -0,0 +1,14 @@
+package com.berry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zlinfo on 2025/1/24
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/berry/server/utils/ApplicationUtil.java
@@ -0,0 +1,7 @@
+package com.berry.server.utils;
+
+/**
+ * Created by zlinfo on 2025/1/24
+ */
+ public class ApplicationUtil {
+}
